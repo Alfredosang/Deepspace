aaa44b47b22a1db4021b0e5d7716f88337596b9d
Alfredosang <osangalfred@gmail.com>
2024-08-14 10:52:52 -0700

    Slider update Metal work

--- components/Metalworks.jsx
+++ components/Metalworks.jsx
@@ -8,13 +8,13 @@ import Link from 'next/link';
 
 const Metalworks = () => {
     return (
-        <div className=' container mx-auto'>
+        <div className=' container justify-center items-center mx-auto'>
             <Carousel
                 additionalTransfrom={0}
                 arrows={false}
                 autoPlaySpeed={3000}
                 centerMode={false}
-                className=""
+                className=" pb-5"
                 containerClass="container"
                 dotListClass=""
                 draggable
@@ -72,7 +72,7 @@ const Metalworks = () => {
                     </div>
 
                     <div className='w-[380px] md:w-[657px] md:ml-10'>
-                        <div className=' pt-2'>
+                        <div className='px-3 md:px-0 pt-2'>
                             <p className='font-body font-[600] text-secondary-100 text-[16px]'>Our Services</p>
                             <h1 className='font-body font-[600] text-black text-[40px] md:text-[64px]'>Metal Work</h1>
                             <p className='font-body font-[600] text-black text-[14px] md:text-[16px] max-w-[533px] '>Are you Looking for expert fabrication and welding services to create machinery, tools, or custom parts? At DeepSpace Africa, we specialize in providing precise solutions tailored to your needs.</p>
@@ -97,7 +97,7 @@ const Metalworks = () => {
                     </div>
 
                     <div className='w-[380px] md:w-[657px] md:ml-10'>
-                        <div className=' pt-2'>
+                        <div className='px-3 md:px-0 pt-2'>
                             <p className='font-body font-[600] text-secondary-100 text-[16px]'>Our Services</p>
                             <h1 className='font-body font-[600] text-black text-[40px] md:text-[64px]'>Wood Work</h1>
                             <p className='font-body font-[600] text-black text-[14px] md:text-[16px] max-w-[533px]'>Are you Looking for expert fabrication and Are you looking to transform your space with custom woodwork! At DeepSpace Africa, we turn your woodwork dreams into reality. Our skilled craftsmen are ready to create stunning pieces tailored to your vision.From beautiful benches and tables to unique chairs, shelves, and wall art, we bring your designs to life with care and precision.</p>
@@ -122,7 +122,7 @@ const Metalworks = () => {
                     </div>
 
                     <div className='w-[380px] md:w-[657px] md:ml-10'>
-                        <div className=' pt-2'>
+                        <div className='px-3 md:px-0 pt-2'>
                             <p className='font-body font-[600] text-secondary-100 text-[16px]'>Our Services</p>
                             <h1 className='font-body font-[600] text-black text-[40px] md:text-[64px]'>3D Printing</h1>
                             <p className='font-body font-[600] text-black text-[14px] md:text-[16px] max-w-[533px]'>Do you have a project in mind? Need custom gifts like keyholders, figurines, phone stands, or other 3D printed items in any quantity? Do you need to 3D print your school projects or broken machine parts? Our 3D printing services enable quick turnaround times, precise details, and cost-effective production.</p>
