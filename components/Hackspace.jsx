6b138c3807bef9bbcc398e979c0d62e5c1467d01
Alfredosang <osangalfred@gmail.com>
2024-08-14 12:46:41 -0700

    Project Slider Update

--- components/Hackspace.jsx
+++ components/Hackspace.jsx
@@ -6,15 +6,15 @@ import { LuSettings2 } from "react-icons/lu";
 
 const Hackspace = () => {
     return (
-        <div className='bg-primary h-[681px]'>
+        <div className='bg-primary h-[700px] md:h-[681px]'>
             <div className='container mx-auto align-middle'>
-                <div className='flex flex-wrap pt-[100px] justify-center items-center'>
+                <div className='flex flex-wrap-reverse pt-[100px] justify-center items-center'>
 
 
-                    <div>
+                    <div className='space-y-3 md:space-y-0'>
                         <p className='text-white font-body text-[16px] font-[500]'>Deep Hackerspace</p>
-                        <h2 className='text-white font-body text-[60px] font-[700]'>Deep Hackerspace</h2>
-                        <p className='text-white font-body text-[16px] font-[400] max-w-[587px]'>Deep MakerSpace is set to be the face of a new wave of innovation and
+                        <h2 className='text-white font-body text-[24px] md:text-[60px] font-[700]'>Deep Hackerspace</h2>
+                        <p className='text-white font-body text-[12px] md:text-[16px] font-[400] w-[335px] md:w-[587px]'>Deep MakerSpace is set to be the face of a new wave of innovation and
                             technology by creating a community where programmers, electronics, tinkerers,
                             and tech enthusiasts can meet to pursue their curiosity and build products.</p>
 
@@ -25,22 +25,22 @@ const Hackspace = () => {
                     </div>
 
 
-                    <div className=' relative flex gap-2 justify-center items-center'>
+                    <div className=' relative flex gap-2 justify-center items-center mb-5' >
                         <div>
-                            <div className='bg-black md:w-[350px] md:h-[271px] rounded-[16px] overflow-hidden border-solid border-secondary-100 border-[2px]'>
+                            <div className='bg-black w-[218px] h-[170px] md:w-[350px] md:h-[271px] rounded-[16px] overflow-hidden border-solid border-secondary-100 border-[2px]'>
                                 <div className='relative w-full h-full'>
 
                                     <Image src="/assets/IM1.jpeg" layout="fill" objectFit="cover" className="object-cover" alt="IM1" />
                                 </div>
                             </div>
                             <div className='flex gap-2 pt-3'>
-                                <div className='w-[170.2px] h-[40px] rounded-full border-solid border-white border-[2px] flex gap-2 items-center justify-center'>
-                                    <div className='w-[24px] h-[24px] bg-secondary-100 rounded-full flex items-center justify-center'>
-                                        <RiHourglass2Fill className='text-white' />
+                                <div className='w-[107px] h-[25px] md:w-[170.2px] md:h-[40px] rounded-full border-solid border-white border-[2px] flex gap-2 items-center justify-center'>
+                                    <div className=' w-[15px] h-[15px] md:w-[24px] md:h-[24px] bg-secondary-100 rounded-full flex items-center justify-center'>
+                                        <RiHourglass2Fill className='text-white text-[10px] md:text-[15px]' />
                                     </div>
-                                    <p className='text-white font-body text-[12px] font-[600]'>Deepspace Incubator</p>
+                                    <p className='text-white font-body text-[7px] md:text-[12px] font-[600]'>Deepspace Incubator</p>
                                 </div>
-                                <div className='bg-black md:w-[172px] md:h-[117px] rounded-[16px] overflow-hidden'>
+                                <div className='bg-black w-[108px] h-[73px] md:w-[172px] md:h-[117px] rounded-[16px] overflow-hidden'>
                                     <div className='relative w-full h-full'>
 
                                         <Image src="/assets/IM2.jpeg" layout="fill" objectFit="cover" className="object-cover filter grayscale" alt="IM2" />
@@ -50,42 +50,42 @@ const Hackspace = () => {
                             </div>
                         </div>
 
-                        <div className='flex flex-col gap-4'>
-                            <div className='bg-black md:w-[172px] md:h-[139px] rounded-[16px] overflow-hidden'>
+                        <div className='flex flex-col gap-2 md:gap-4'>
+                            <div className='bg-black w-[108px] h-[87px] md:w-[172px] md:h-[139px] rounded-[16px] overflow-hidden'>
                                 <div className='relative w-full h-full'>
 
                                     <Image src="/assets/IM3.jpeg" layout="fill" objectFit="cover" className="object-cover filter grayscale" alt="IM3" />
                                 </div>
                             </div>
 
-                            <div className='w-[170.2px] h-[40px] rounded-full border-solid border-white border-[2px] flex gap-2 items-center justify-center'>
-                                <div className='w-[24px] h-[24px] bg-secondary-100 rounded-full flex items-center justify-center'>
-                                    <LuSettings2 className='text-white' />
+                            <div className='w-[107px] h-[25px] md:w-[170.2px] md:h-[40px] rounded-full border-solid border-white border-[2px] flex gap-2 items-center justify-center'>
+                                <div className=' w-[15px] h-[15px] md:w-[24px] md:h-[24px] bg-secondary-100 rounded-full flex items-center justify-center'>
+                                    <LuSettings2 className='text-white text-[10px] md:text-[15px]' />
                                 </div>
-                                <p className='text-white font-body text-[12px] font-[600]'>Deep Space Learning</p>
+                                <p className='text-white font-body text-[7px] md:text-[12px] font-[600]'>Deep Space Learning</p>
                             </div>
 
-                            <div className='bg-black md:w-[172px] md:h-[139px] rounded-[16px] overflow-hidden'>
+                            <div className='bg-black w-[108px] h-[87px] md:w-[172px] md:h-[139px] rounded-[16px] overflow-hidden'>
                                 <div className='relative w-full h-full'>
 
                                     <Image src="/assets/IM4.jpeg" layout="fill" objectFit="cover" className="object-cover filter grayscale" alt="IM4" />
                                 </div>
                             </div>
 
-                            <div className='w-[170.2px] h-[40px] rounded-full border-solid border-white border-[2px] flex gap-2 items-center justify-center'>
-                                <div className='w-[24px] h-[24px] bg-secondary-100 rounded-full flex items-center justify-center'>
-                                    <LuSettings2 className='text-white' />
+                            <div className='w-[107px] h-[25px] md:w-[170.2px] md:h-[40px] rounded-full border-solid border-white border-[2px] flex gap-2 items-center justify-center'>
+                                <div className=' w-[15px] h-[15px] md:w-[24px] md:h-[24px] bg-secondary-100 rounded-full flex items-center justify-center'>
+                                    <LuSettings2 className='text-white text-[10px] md:text-[15px]' />
                                 </div>
-                                <p className='text-white font-body text-[12px] font-[600]'>Deep Makerspace</p>
+                                <p className='text-white font-body text-[7px] md:text-[12px] font-[600]'>Deep Makerspace</p>
                             </div>
                         </div>
 
-                        <div className=' absolute top-[-10px] right-[120px] w-[170.2px] h-[40px] rounded-full border-solid bg-white border-secondary-100 border-[1px] flex gap-2 items-center justify-center'>
-                                <div className='w-[24px] h-[24px] bg-secondary-100 rounded-full flex items-center justify-center'>
-                                    <LuSettings2 className='text-white' />
-                                </div>
-                                <p className='text-black font-body text-[12px] font-[600]'>Deep Makerspace</p>
+                        <div className=' absolute top-[-10px] right-[120px] w-[107px] h-[25px] md:w-[170.2px] md:h-[40px] rounded-full border-solid bg-white border-secondary-100 border-[1px] flex gap-2 items-center justify-center'>
+                            <div className='w-[15px] h-[15px] md:w-[24px] md:h-[24px] bg-secondary-100 rounded-full flex items-center justify-center'>
+                                <LuSettings2 className='text-white text-[10px] md:text-[15px]' />
                             </div>
+                            <p className='text-black font-body text-[7px] md:text-[12px] font-[600]'>Deep Makerspace</p>
+                        </div>
 
                     </div>
 
--- components/Projectgallery.jsx
+++ components/Projectgallery.jsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 import Slider from "react-slick";
-import React from 'react'
+import React from 'react';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Image from "next/image";
@@ -14,11 +14,12 @@ const Projectgallery = () => {
         slidesToShow: 5,
         slidesToScroll: 1,
         autoplay: true,
+        mobileFirst: true,
         responsive: [
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
+                    slidesToShow: 5,
                     slidesToScroll: 1,
                     infinite: true,
                     dots: true
@@ -27,7 +28,7 @@ const Projectgallery = () => {
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 2,
+                    slidesToShow: 3,
                     slidesToScroll: 1
                 }
             },
@@ -35,48 +36,48 @@ const Projectgallery = () => {
                 breakpoint: 480,
                 settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 1
+                    slidesToScroll: 1,
+                    dots: false
                 }
             }
-
         ]
     };
 
     return (
-        <div className="container mx-auto">
+        <div className="container mx-auto ">
             <div className="mb-16">
                 <div className="my-10">
                     <div className="">
-                    <p className="text-secondary-100 font-body text-[16px] font-[600] text-center">Projects Done</p>
-                    <h1 className="text-black font-body text-[40px] font-[700] text-center">Projects Gallery</h1>
+                        <p className="text-secondary-100 font-body text-[16px] font-[600] text-center">Projects Done</p>
+                        <h1 className="text-black font-body text-[40px] font-[700] text-center">Projects Gallery</h1>
                     </div>
                 </div>
 
-
-                <Slider {...settings}>
-                    
-                    <div className="rounded-[12px] overflow-hidden w-[298px] h-[370px]">
-                        <Image src="/assets/pg2.png" width={298} height={370} alt="Project image" />
+                <Slider className="flex justify-center items-center gap-3" {...settings}>
+                    <div className="flex justify-center">
+                        <div className="rounded-[12px] overflow-hidden w-[298px] h-[370px] mx-auto">
+                            <Image src="/assets/pg2.png" width={298} height={370} alt="Project image" />
+                        </div>
                     </div>
-                    <div className="rounded-[12px] overflow-hidden w-[298px] h-[370px]">
-                        <Image src="/assets/pg3.png" width={298} height={370} alt="Project image" />
+                    <div className="flex justify-center">
+                        <div className="rounded-[12px] overflow-hidden w-[298px] h-[370px] mx-auto">
+                            <Image src="/assets/pg3.png" width={298} height={370} alt="Project image" />
+                        </div>
                     </div>
-                    
-                    <div className="rounded-[12px] overflow-hidden w-[298px] h-[370px]">
-                        <Image src="/assets/pg55.png" width={298} height={370} alt="Project image" />
+                    <div className="flex justify-center">
+                        <div className="rounded-[12px] overflow-hidden w-[298px] h-[370px] mx-auto">
+                            <Image src="/assets/pg55.png" width={298} height={370} alt="Project image" />
+                        </div>
                     </div>
-
-                    <div className="rounded-[12px] overflow-hidden w-[298px] h-[370px]">
-                        <Image src="/assets/pg6.png" width={298} height={370} alt="Project image" />
+                    <div className="flex justify-center">
+                        <div className="rounded-[12px] overflow-hidden w-[298px] h-[370px] mx-auto">
+                            <Image src="/assets/pg6.png" width={298} height={370} alt="Project image" />
+                        </div>
                     </div>
-                    
-
-
                 </Slider>
             </div>
-
         </div>
     )
 }
 
-export default Projectgallery
+export default Projectgallery;
